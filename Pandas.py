a12be92736d85077b58421cc31d3fa238a87cf29
Shariph7 <shariphthapa123@gmail.com>
2024-03-02 21:18:09 +0545

    PANDAS IN PYTHON

---
+++ Pandas.py
@@ -0,0 +1,19 @@
+# Machine Learning - is the process of making machine learn from the data and statistics and predicting the outcome.
+# for that pandas plays the vital role for the analysis of the data.
+# Pandas stands for Python Data Analaysis or Panel Data
+
+'''
+    Simple Steps of Data Visualization
+
+    Data(Collection) -> Data Ware House(Database) -> Analayis(Pandas) -> Result(outcome) -> Visualization(Python Matplot Libraries)
+'''
+
+# What is Python Pandas?
+# --> Python Pandas is the data analaysis tools for analyazing the reports and visulizing it which is itselt written in C/C++
+
+# Types of the Pandas are:
+'''
+    1.> Series(1 Dimensional DataFrame)
+    2.> DataFrame(2 Dimensional Data Frame)
+    3.> Panel Data(3 Dimenstional Data Frame)
+'''
